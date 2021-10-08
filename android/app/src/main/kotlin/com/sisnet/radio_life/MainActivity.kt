b017a2fba6fa94393276627db443cc1fc5bb6906
Denis Viana <denisvcosta@gmail.com>
2021-10-08 10:52:03 -0300

    Fix android module; UI adjustments

--- android/app/src/main/kotlin/com/sisnet/radio_life/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.sisnet.radio_life
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/app/pages/super_hero_details/super_hero_details_page.dart
+++ lib/app/pages/super_hero_details/super_hero_details_page.dart
@@ -36,158 +36,160 @@ class SuperHeroDetailsPage extends GetView<SuperHeroDetailsController> {
               final model = controller.state.value.data;
               return model == null
                   ? Container()
-                  : SingleChildScrollView(
-                      child: Column(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(AppSpacing.medium),
-                            child: Row(
-                              children: [
-                                Hero(
-                                  tag: model.id,
-                                  child: CachedNetworkImage(
-                                    imageUrl: model.image,
-                                    fit: BoxFit.cover,
-                                    height: 200,
-                                  ),
+                  : Column(
+                    children: [
+                      Flexible(
+                        flex: 1,
+                        child: Padding(
+                          padding: const EdgeInsets.all(AppSpacing.medium),
+                          child: Row(
+                            children: [
+                              Hero(
+                                tag: model.id,
+                                child: CachedNetworkImage(
+                                  imageUrl: model.image,
+                                  fit: BoxFit.cover,
+                                  height: 200,
                                 ),
-                                UIHelper.horizontalSpaceMedium,
-                                Flexible(
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    children: [
-                                      AutoSizeText(
-                                        model.name,
-                                        maxLines: 1,
-                                        style: TextStyle(
-                                            fontSize: AppFontSize.ultra,
-                                            color: AppColorScheme.emphasis,
-                                            fontWeight: AppFontWeight.bold),
-                                      ),
-                                      UIHelper.verticalSpaceSmall,
-                                      Container(
-                                        decoration: BoxDecoration(
-                                            border: Border.all(
-                                                color: AppColorScheme.border,
-                                                width: 1)),
-                                      ),
-                                      UIHelper.verticalSpaceSmall,
-                                      Text(
-                                        S.of(context).powerStats,
-                                        style: TextStyle(
-                                            fontSize: AppFontSize.small,
-                                            color: AppColorScheme.emphasis),
-                                      ),
-                                      UIHelper.verticalSpaceSmall,
-                                      Text(
-                                        'Power: ${model.power}',
-                                        style: TextStyle(
-                                            fontSize: AppFontSize.small,
-                                            color: AppColorScheme.emphasis),
-                                      ),
-                                      Text(
-                                        'Durability: ${model.durability}',
-                                        style: TextStyle(
-                                            fontSize: AppFontSize.small,
-                                            color: AppColorScheme.emphasis),
-                                      ),
-                                      Text(
-                                        'Speed: ${model.speed}',
-                                        style: TextStyle(
-                                            fontSize: AppFontSize.small,
-                                            color: AppColorScheme.emphasis),
-                                      ),
-                                      Text(
-                                        'Intelligence: ${model.intelligence}',
-                                        style: TextStyle(
-                                            fontSize: AppFontSize.small,
-                                            color: AppColorScheme.emphasis),
-                                      ),
-                                      Text(
-                                        'Combat: ${model.combat}',
-                                        style: TextStyle(
-                                            fontSize: AppFontSize.small,
-                                            color: AppColorScheme.emphasis),
-                                      ),
-                                      Text(
-                                        'Strength: ${model.strength}',
-                                        style: TextStyle(
-                                            fontSize: AppFontSize.small,
-                                            color: AppColorScheme.emphasis),
-                                      ),
-                                    ],
-                                  ),
+                              ),
+                              UIHelper.horizontalSpaceMedium,
+                              Flexible(
+                                child: Column(
+                                  crossAxisAlignment:
+                                      CrossAxisAlignment.start,
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    AutoSizeText(
+                                      model.name,
+                                      maxLines: 1,
+                                      style: TextStyle(
+                                          fontSize: AppFontSize.ultra,
+                                          color: AppColorScheme.emphasis,
+                                          fontWeight: AppFontWeight.bold),
+                                    ),
+                                    UIHelper.verticalSpaceSmall,
+                                    Container(
+                                      decoration: BoxDecoration(
+                                          border: Border.all(
+                                              color: AppColorScheme.border,
+                                              width: 1)),
+                                    ),
+                                    UIHelper.verticalSpaceSmall,
+                                    Text(
+                                      S.of(context).powerStats,
+                                      style: TextStyle(
+                                          fontSize: AppFontSize.small,
+                                          color: AppColorScheme.emphasis),
+                                    ),
+                                    UIHelper.verticalSpaceSmall,
+                                    Text(
+                                      'Power: ${model.power}',
+                                      style: TextStyle(
+                                          fontSize: AppFontSize.small,
+                                          color: AppColorScheme.emphasis),
+                                    ),
+                                    Text(
+                                      'Durability: ${model.durability}',
+                                      style: TextStyle(
+                                          fontSize: AppFontSize.small,
+                                          color: AppColorScheme.emphasis),
+                                    ),
+                                    Text(
+                                      'Speed: ${model.speed}',
+                                      style: TextStyle(
+                                          fontSize: AppFontSize.small,
+                                          color: AppColorScheme.emphasis),
+                                    ),
+                                    Text(
+                                      'Intelligence: ${model.intelligence}',
+                                      style: TextStyle(
+                                          fontSize: AppFontSize.small,
+                                          color: AppColorScheme.emphasis),
+                                    ),
+                                    Text(
+                                      'Combat: ${model.combat}',
+                                      style: TextStyle(
+                                          fontSize: AppFontSize.small,
+                                          color: AppColorScheme.emphasis),
+                                    ),
+                                    Text(
+                                      'Strength: ${model.strength}',
+                                      style: TextStyle(
+                                          fontSize: AppFontSize.small,
+                                          color: AppColorScheme.emphasis),
+                                    ),
+                                  ],
                                 ),
-                              ],
-                            ),
+                              ),
+                            ],
                           ),
-                          Container(
-                            padding: const EdgeInsets.all(AppSpacing.medium),
-                            decoration: BoxDecoration(
-                                borderRadius: const BorderRadius.vertical(
-                                  top: Radius.circular(30),
-                                ),
-                                color: AppColorScheme.backgroundLight),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                UIHelper.verticalSpaceSmall,
-                                Text(
-                                  S.of(context).biography,
-                                  style: TextStyle(
-                                      fontSize: AppFontSize.mega,
-                                      color: AppColorScheme.emphasis),
-                                ),
-                                UIHelper.verticalSpaceSmall,
-                                Text(
-                                  'Full name: ${model.fullName}',
-                                  style: TextStyle(
-                                      color: AppColorScheme.emphasis),
-                                ),
-                                UIHelper.verticalSpaceSmall,
-                                Text(
-                                  'Alter egos: ${model.alterEgos}',
-                                  style: TextStyle(
-                                      color: AppColorScheme.emphasis),
-                                ),
-                                UIHelper.verticalSpaceSmall,
-                                Text(
-                                  'Aliases: ${model.aliases}',
-                                  style: TextStyle(
-                                      color: AppColorScheme.emphasis),
-                                ),
-                                UIHelper.verticalSpaceSmall,
-                                Text(
-                                  'Place of birth: ${model.placeOfBirth}',
-                                  style: TextStyle(
-                                      color: AppColorScheme.emphasis),
-                                ),
-                                UIHelper.verticalSpaceSmall,
-                                Text(
-                                  'First Appearance: ${model.firstAppearance}',
-                                  style: TextStyle(
-                                      color: AppColorScheme.emphasis),
-                                ),
-                                UIHelper.verticalSpaceSmall,
-                                Text(
-                                  'Publisher: ${model.publisher}',
-                                  style: TextStyle(
-                                      color: AppColorScheme.emphasis),
-                                ),
-                                UIHelper.verticalSpaceSmall,
-                                Text(
-                                  'Alignment: ${model.alignment}',
-                                  style: TextStyle(
-                                      color: AppColorScheme.emphasis),
-                                ),
-                              ],
-                            ),
-                          )
-                        ],
+                        ),
                       ),
-                    );
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        padding: const EdgeInsets.all(AppSpacing.medium),
+                        decoration: BoxDecoration(
+                            borderRadius: const BorderRadius.vertical(
+                              top: Radius.circular(30),
+                            ),
+                            color: AppColorScheme.backgroundLight),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            UIHelper.verticalSpaceSmall,
+                            Text(
+                              S.of(context).biography,
+                              style: TextStyle(
+                                  fontSize: AppFontSize.mega,
+                                  color: AppColorScheme.emphasis),
+                            ),
+                            UIHelper.verticalSpaceMedium,
+                            Text(
+                              'Full name: ${model.fullName}',
+                              style: TextStyle(
+                                  color: AppColorScheme.emphasis),
+                            ),
+                            UIHelper.verticalSpaceSmall,
+                            Text(
+                              'Alter egos: ${model.alterEgos}',
+                              style: TextStyle(
+                                  color: AppColorScheme.emphasis),
+                            ),
+                            UIHelper.verticalSpaceSmall,
+                            Text(
+                              'Aliases: ${model.aliases}',
+                              style: TextStyle(
+                                  color: AppColorScheme.emphasis),
+                            ),
+                            UIHelper.verticalSpaceSmall,
+                            Text(
+                              'Place of birth: ${model.placeOfBirth}',
+                              style: TextStyle(
+                                  color: AppColorScheme.emphasis),
+                            ),
+                            UIHelper.verticalSpaceSmall,
+                            Text(
+                              'First Appearance: ${model.firstAppearance}',
+                              style: TextStyle(
+                                  color: AppColorScheme.emphasis),
+                            ),
+                            UIHelper.verticalSpaceSmall,
+                            Text(
+                              'Publisher: ${model.publisher}',
+                              style: TextStyle(
+                                  color: AppColorScheme.emphasis),
+                            ),
+                            UIHelper.verticalSpaceSmall,
+                            Text(
+                              'Alignment: ${model.alignment}',
+                              style: TextStyle(
+                                  color: AppColorScheme.emphasis),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  );
             }),
           ),
         ),
